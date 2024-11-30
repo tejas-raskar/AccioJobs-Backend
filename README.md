f059e5cfe10bee2a4d0478f02362747f276ed6b6
Tejas Raskar <tjsrskr@gmail.com>
2024-11-30 12:23:37 +0530

    Added api fetch logic

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # AccioJobs-Backend
 Backend for the project AccioJobs
+
+### Introduction
+AccioJobs-Backend is the backend service for the AccioJobs project. Its main objective is to fetch job listings from various APIs like Remotive and RemoteOK, as well as from various RSS feeds. The collected data is then combined and pushed to the database.
+
+## Features
+- Fetch jobs from Remotive API
+- Fetch jobs from RemoteOK API
+- Fetch jobs from various RSS feeds
+- Combine and store job data in the database
+
+## License
+This project is licensed under the GPL-3.0 License.
---
+++ scripts/fetch_jobs_api.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+
+# cleans the api response to better structure the db 
+def clean_jobs(job, key_mappings):
+    cleaned_jobs = {}
+    for key, api_key in key_mappings.items():
+        if key == "description":
+            parsed_description = BeautifulSoup(job.get(api_key), 'html_parser')
+            cleaned_jobs[key] = parsed_description.get_text()
+        else:
+            cleaned_jobs[key] = job.get(api_key)
+    return cleaned_jobs
+
+# key_mappings - maps the key used by the api to a single common key to use across the projct
+remoteok_key_mapping = {
+    "postedOn": "date",
+    "company": "company",
+    "company_logo": "company_logo",
+    "position": "position",
+    "tags": "tags",
+    "salary": "salary_min",
+    "location": "location",
+    "apply_url": "apply_url",
+    "description": "description"
+}
+
+remotive_key_mapping = {
+    "postedOn": "publication_data",
+    "company": "company_name",
+    "company_logo": "company_logo",
+    "position": "position",
+    "tags": "tags",
+    "salary": "salary",
+    "location": "candidate_required_location",
+    "apply_url": "apply_url",
+    "description": "description"
+}
+
+remotive_response = requests.get("https://remotive.com/api/remote-jobs")
+remotive_jobs = remotive_response.json().get("jobs")
+remotive_jobs = remotive_jobs[2:]
+cleaned_remotive_jobs = [clean_jobs(job, remotive_key_mapping) for job in remotive_jobs]
+
+remoteok_response = requests.get("https://remotive.com/api/remote-jobs")
+remoteok_jobs = remoteok_response.json()
+remoteok_jobs = remoteok_jobs[1:]
+cleaned_remoteok_jobs = [clean_jobs(job, remoteok_key_mapping) for job in remoteok_jobs]
+
+# commbines jobs from both the sources into a single objects
+all_jobs = cleaned_remotive_jobs + cleaned_remoteok_jobs

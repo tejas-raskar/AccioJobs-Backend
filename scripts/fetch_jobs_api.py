427b10aaf4e8b015daaa0bdda61aac5c80fa36fc
Tejas Raskar <tjsrskr@gmail.com>
2024-11-30 13:47:16 +0530

    Fixed wrong key mappings

--- scripts/fetch_jobs_api.py
+++ scripts/fetch_jobs_api.py
@@ -6,7 +6,7 @@ def clean_jobs(job, key_mappings):
     cleaned_jobs = {}
     for key, api_key in key_mappings.items():
         if key == "description":
-            parsed_description = BeautifulSoup(job.get(api_key), 'html_parser')
+            parsed_description = BeautifulSoup(job.get(api_key), 'html.parser')
             cleaned_jobs[key] = parsed_description.get_text()
         else:
             cleaned_jobs[key] = job.get(api_key)
@@ -26,14 +26,14 @@ remoteok_key_mapping = {
 }
 
 remotive_key_mapping = {
-    "postedOn": "publication_data",
+    "postedOn": "publication_date",
     "company": "company_name",
     "company_logo": "company_logo",
-    "position": "position",
+    "position": "title",
     "tags": "tags",
     "salary": "salary",
     "location": "candidate_required_location",
-    "apply_url": "apply_url",
+    "apply_url": "url",
     "description": "description"
 }
 
@@ -42,7 +42,7 @@ remotive_jobs = remotive_response.json().get("jobs")
 remotive_jobs = remotive_jobs[2:]
 cleaned_remotive_jobs = [clean_jobs(job, remotive_key_mapping) for job in remotive_jobs]
 
-remoteok_response = requests.get("https://remotive.com/api/remote-jobs")
+remoteok_response = requests.get("https://remoteok.com/api")
 remoteok_jobs = remoteok_response.json()
 remoteok_jobs = remoteok_jobs[1:]
 cleaned_remoteok_jobs = [clean_jobs(job, remoteok_key_mapping) for job in remoteok_jobs]
